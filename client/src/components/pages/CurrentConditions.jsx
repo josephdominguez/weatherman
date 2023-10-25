f37d18be0de1041724907284c5156b4dfe1c9b7d
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-25 17:02:35 -0600

    added dynamic api rendering to current conditions

--- client/src/components/pages/CurrentConditions.jsx
+++ client/src/components/pages/CurrentConditions.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import AppHeader from "@components/headers/AppHeader";
 import AppFooter from "@components/footers/AppFooter";
 import styles from "@css/current_conditions.module.css";
@@ -15,23 +16,20 @@ function CurrentConditions() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      const apiData = {
-        temperature: "63°F",
-        condition: "Rainy",
-        conditionIcon: "images/conditions/shower.gif",
-        wind: "Calm",
-        location: "Salt Lake City",
-        humidity: "63%",
-        dewpoint: "70°F",
-        ceiling: "Unlimited",
-        visibility: "10 mi.",
-        pressure: "29.99 S",
-        heatIndex: "89°",
-      };
-      setWeatherData(apiData);
-      setLoading(false);
-    }, 2000); // Simulates a 2-second delay
+    const getCurrentConditions = async () => {
+      try {
+        // replace url with hosted api
+        const response = await axios.get('http://localhost:8080/current-conditions?zipCode=84101');
+        const data = response.data;
+        setWeatherData(data.currentConditions);
+        setLoading(false);
+      } catch (e) {
+        setError(e);
+        setLoading(false);
+      }
+    };
+
+    getCurrentConditions();
   }, []);
 
   if (loading) {
@@ -50,14 +48,15 @@ function CurrentConditions() {
           <div className={`${styles["current-conditions-container"]} gradient-border`}>
             <div className={styles["current-conditions-item"]}>
               <div className={styles["temperature"]}>
-                {weatherData.temperature}
+                {weatherData.temperature} °F
               </div>
-              <div className={styles["align-start"]}>
+              <div className={styles["condition-container"]}>
                 <div>{weatherData.condition}</div>
-                <img className={styles["condition"]} src={weatherData.conditionIcon} alt={weatherData.condition} />
+                <img className={styles["condition-icon"]} src={weatherData.conditionIcon} alt={weatherData.condition} />
               </div>
-              <div className={styles["align-start"]}>
-                <WeatherInfo label="Wind" value={weatherData.wind} />
+                
+              <div className={styles["wind-container"]}>
+                <WeatherInfo label="Wind" value={`${weatherData.wind} MPH`} />
               </div>
             </div>
             <div className={styles["current-conditions-item"]}>
@@ -65,11 +64,11 @@ function CurrentConditions() {
                 <div className={styles["details-heading"]}>
                   <span className="highlight"> {weatherData.location} </span>
                 </div>
-                <WeatherInfo label="Humidity" value={weatherData.humidity} />
-                <WeatherInfo label="Dewpoint" value={weatherData.dewpoint} />
+                <WeatherInfo label="Humidity" value={`${weatherData.humidity} %`} />
+                <WeatherInfo label="Dewpoint" value={`${weatherData.dewpoint}°F`}/>
                 <WeatherInfo label="Ceiling" value={weatherData.ceiling} />
                 <WeatherInfo label="Visibility" value={weatherData.visibility} />
-                <WeatherInfo label="Pressure" value={weatherData.pressure} />
+                <WeatherInfo label="Pressure" value={`${weatherData.pressure} S`} />
                 <WeatherInfo label="Heat Index" value={weatherData.heatIndex} />
               </div>
             </div>
