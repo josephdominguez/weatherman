93b08f9aaf7f7b7add9a631f4650d9f284cba1f3
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-26 01:22:51 -0600

    possible version of api using nws

--- server/app/models/condition-icons.model.js
+++ server/app/models/condition-icons.model.js
@@ -1,6 +1,7 @@
 const conditionIcons = {
     Clear: 'images/conditions/clear_day.gif',
     Clouds: 'images/conditions/cloudy.gif',
+    'Chance Rain And Snow': 'images/conditions/cloudy.gif',
 }
 
 module.exports = conditionIcons;
