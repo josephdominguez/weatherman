ac5e18152ce90b04048618cfa967cd66c044825f
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-26 18:53:49 -0600

    added state for current location

--- client/src/components/headers/AppHeader.jsx
+++ client/src/components/headers/AppHeader.jsx
@@ -1,33 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from '@css/app/header.module.css';
-import WeathermanLogo from '@images/logos/logo.svg';
-import '@scripts/clock.js';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styles from "@css/app/header.module.css";
+import WeathermanLogo from "@images/logos/logo.svg";
+import "@scripts/clock.js";
 
 function AppHeader({ pageTitle, location }) {
+  const [currentLocation, setCurrentLocation] = useState(location);
+
+  useEffect(() => {
+    setCurrentLocation(location);
+  }, [location]);
+
   return (
-    <header className={styles['app-header']}>
-        <div className={styles['header-container']}>
-            <div className={styles['header-item']}>
-                <div>
-                    <Link to="/">
-                        <img className={styles['logo']} src={WeathermanLogo} alt="Weather Logo" />
-                    </Link>
-                </div>
-                <div>
-                    <div> {location}'s </div>
-                    <div> {pageTitle} </div>
-                </div>
-
-            </div>
-
-            <div className={styles['header-item']}>
-                <div className={styles['clock']}>
-                    <div id="time">00:00:00 AM</div>
-                    <div id="date">SUN JAN 1</div>
-                </div>
-            </div>
+    <header className={styles["app-header"]}>
+      <div className={styles["header-container"]}>
+        <div className={styles["header-item"]}>
+          <div>
+            <Link to="/">
+              <img
+                className={styles["logo"]}
+                src={WeathermanLogo}
+                alt="Weather Logo"
+              />
+            </Link>
+          </div>
+          <div>
+            {currentLocation && (
+              <>
+                <div>{currentLocation}'s</div>
+                <div>{pageTitle}</div>
+              </>
+            )}
+            {!currentLocation && (
+              <>
+                <div>{pageTitle}</div>
+              </>
+            )}
+          </div>
         </div>
+
+        <div className={styles["header-item"]}>
+          <div className={styles["clock"]}>
+            <div id="time">0:00:00 AM</div>
+            <div id="date">Sun, Jan 1</div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
