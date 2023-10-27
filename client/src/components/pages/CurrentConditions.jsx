fff870fd8aca2d0868778a35a0177462ac4ca319
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-26 18:54:34 -0600

    added separated components

--- client/src/components/pages/CurrentConditions.jsx
+++ client/src/components/pages/CurrentConditions.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import AppHeader from '@components/headers/AppHeader';
-import AppFooter from '@components/footers/AppFooter';
+import AppPage from '@components/pages/AppPage';
+import ErrorComponent from '@components/app/ErrorComponent';
+import LoadingComponent from '@components/app/LoadingComponent';
 import styles from '@css/current_conditions.module.css';
 import { ZipCodeContext } from '@contexts/ZipCodeContext';
 
-const WeatherInfo = ({ label, value, unit, highlightUnit}) => (
+// WeatherInfo component for displaying weather information
+const WeatherInfo = ({ label, value, unit, highlightUnit }) => (
   <div>
     {label}: &nbsp; {value} {highlightUnit ? <span className="highlight">{unit}</span> : unit}
   </div>
@@ -15,7 +17,7 @@ function CurrentConditions() {
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const {zipCode, setZipCode} = useContext(ZipCodeContext); // Loads zip code context
+  const { zipCode, setZipCode } = useContext(ZipCodeContext);
 
   const handleZipCodeChange = (event) => {
     setZipCode(event.target.value);
@@ -23,7 +25,6 @@ function CurrentConditions() {
 
   const fetchWeatherData = async () => {
     try {
-      // replace url with hosted api
       const response = await axios.get(`http://localhost:8080/current-conditions?zipCode=${zipCode}`);
       const data = response.data;
       setWeatherData(data.currentConditions);
@@ -35,89 +36,65 @@ function CurrentConditions() {
     }
   };
 
-  useEffect(() => { fetchWeatherData(); }, []); // Fetch data when zipCode Changes
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
 
   if (loading) {
     return (
-      <>
-        <AppHeader pageTitle="Current Conditions" location="" />
-        <main>
-            <div className={styles["current-conditions"]}>
-              <div className={`${styles["current-conditions-container"]} gradient-border`}>
-                <div className={styles["current-conditions-item"]}>
-                    <div>Loading...</div>
-                </div>
-              </div>
-            </div>
-          </main>
-        <AppFooter humidity="" dewpoint="" />
-      </>
+      <AppPage pageTitle="Current Conditions" humidity="" dewpoint="">
+        <LoadingComponent />
+      </AppPage>
     );
   }
 
   if (error) {
     return (
-      <>
-          <AppHeader pageTitle="Current Conditions" location="" />
-          <main>
-            <div className={styles["current-conditions"]}>
-              <div className={`${styles["current-conditions-container"]} gradient-border`}>
-                <div className={styles["current-conditions-item"]}>
-                    <div>Error: {error.message} </div>
-                    <div>
-                      <input type="text" placeholder="Enter Zip Code" value={zipCode} onChange={handleZipCodeChange}/> {' '}
-                      <button onClick={fetchWeatherData}>Get Weather</button>
-                    </div>
-                </div>
-              </div>
-            </div>
-          </main>
-          <AppFooter humidity="" dewpoint="" />
-      </>
+      <AppPage pageTitle="Current Conditions" humidity="" dewpoint="">
+        <ErrorComponent error={error}>
+          <div>
+            <input type="text" placeholder="Enter Zip Code" value={zipCode} onChange={handleZipCodeChange} />{' '}
+            <button onClick={fetchWeatherData}>Get Weather</button>
+          </div>
+        </ErrorComponent>
+      </AppPage>
     );
   }
 
   return (
-    <>
-      <AppHeader pageTitle="Current Conditions" location={weatherData.location}/>
-      <main>
-        <div className={styles["current-conditions"]}>
-          <div className={`${styles["current-conditions-container"]} gradient-border`}>
-            <div className={styles["current-conditions-item"]}>
-              <div className={styles["temperature"]}>
-                {weatherData.temperature}&deg;F
-              </div>
-              <div className={styles["condition-container"]}>
-                <div>{weatherData.condition}</div>
-                <img className={styles["condition-icon"]} src={weatherData.conditionIcon} alt={weatherData.condition} />
-              </div>
-                
-              <div className={styles["wind-container"]}>
-                <WeatherInfo label="Wind" value={`${weatherData.wind} MPH`} />
-              </div>
+    <AppPage pageTitle="Current Conditions" location={weatherData.location} humidity={weatherData.humidity} dewpoint={weatherData.dewpoint}>
+      <div className={`${styles["card-container"]}`}>
+        <div className={styles["card-item"]}>
+          <div className={styles["temperature"]}>
+            {weatherData.temperature}&deg;F
+          </div>
+          <div className={styles["condition-container"]}>
+            <div>{weatherData.condition}</div>
+            <img className={styles["condition-icon"]} src={weatherData.conditionIcon} alt={weatherData.condition} />
+          </div>
+          <div className={styles["wind-container"]}>
+            <WeatherInfo label="Wind" value={`${weatherData.wind} MPH`} />
+          </div>
+        </div>
+        <div className={styles["card-item"]}>
+          <div className={styles["details"]}>
+            <div className={styles["details-heading"]}>
+              <span className="highlight"> {weatherData.location} </span>
             </div>
-            <div className={styles["current-conditions-item"]}>
-              <div className={styles["details"]}>
-                <div className={styles["details-heading"]}>
-                  <span className="highlight"> {weatherData.location} </span>
-                </div>
-                <WeatherInfo label="Humidity" value={weatherData.humidity} unit="%" />
-                <WeatherInfo label="Dewpoint" value={weatherData.dewpoint} unit="&deg;F" />
-                <WeatherInfo label="Ceiling" value={weatherData.ceiling} unit="" />
-                <WeatherInfo label="Visibility" value={weatherData.visibility} unit="mi." />
-                <WeatherInfo label="Pressure" value={weatherData.pressure} unit="mb" highlightUnit />
-                <WeatherInfo label="Heat Index" value={weatherData.heatIndex} unit="" />
-                <div>
-                  <input type="text" placeholder="Enter Zip Code" value={zipCode} onChange={handleZipCodeChange}/> {' '}
-                  <button onClick={fetchWeatherData}>Get Weather</button>
-                </div>
-              </div>
+            <WeatherInfo label="Humidity" value={weatherData.humidity} unit="%" />
+            <WeatherInfo label="Dewpoint" value={weatherData.dewpoint} unit="&deg;F" />
+            <WeatherInfo label="Ceiling" value={weatherData.ceiling} unit="" />
+            <WeatherInfo label="Visibility" value={weatherData.visibility} unit="mi." />
+            <WeatherInfo label="Pressure" value={weatherData.pressure} unit="mb" highlightUnit />
+            <WeatherInfo label="Heat Index" value={weatherData.heatIndex} unit="" />
+            <div>
+              <input type="text" placeholder="Enter Zip Code" value={zipCode} onChange={handleZipCodeChange} />{' '}
+              <button onClick={fetchWeatherData}>Get Weather</button>
             </div>
           </div>
         </div>
-      </main>
-      <AppFooter humidity={weatherData.humidity} dewpoint={weatherData.dewpoint} />
-    </>
+      </div>
+    </AppPage>
   );
 }
 
