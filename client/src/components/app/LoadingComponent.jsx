ae3ed38c17aaa97af7c793e9d97ea5d5692049c6
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-26 18:53:24 -0600

    created separate loading component

---
+++ client/src/components/app/LoadingComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '@css/app/card_container.module.css';
+
+function LoadingComponent() {
+  return (
+    <div className={`${styles["card-container"]}`}>
+        <div className={styles["card-item"]}>
+            <div>Loading...</div>
+        </div>
+    </div>
+  );
+}
+
+export default LoadingComponent;
