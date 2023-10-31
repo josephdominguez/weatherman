2a4732a846a2b2a29841ad1b91987af11ce68e7e
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-31 03:18:29 -0600

    added json object representing app pages to config

--- client/config/config.js
+++ client/config/config.js
@@ -1,1 +1,24 @@
-export const API_ENDPOINT='localhost:8080';
+export const API_ENDPOINT = "localhost:8080";
+
+export const pages = [
+    {
+        component: "CompleteForecast",
+        title: "Complete Forecast",
+        path: "/CompleteForecast"
+    },
+    {
+        component: "CurrentConditionsComponent",
+        title: "Current Conditions",
+        path: "/CurrentConditions",
+    },
+    {
+        component: "ExtendedForecastComponent",
+        title: "Extended Forecast",
+        path: "/ExtendedForecast",
+    },
+    {
+        component: "LocalForecastComponent",
+        title: "Local Forecast",
+        path: "/LocalForecast",
+    },
+];
