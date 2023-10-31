9e70e2c56e1b4db99d8f2f5457dd91fc0aa147cc
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-31 03:19:19 -0600

    added lazy importing to cycle component

--- client/src/components/utilities/Cycle.jsx
+++ client/src/components/utilities/Cycle.jsx
@@ -1,23 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, lazy, Suspense } from "react";
+import LoadingComponent from "@components/app/LoadingComponent";
 
-function Cycle({components}) {
-    const [currentComponentIndex, setCurrentComponentIndex] = useState(0);
-    const CurrentComponent = components[currentComponentIndex];
+function Cycle({ components }) {
+  const [currentComponentIndex, setCurrentComponentIndex] = useState(0);
+  const CurrentComponent = components[currentComponentIndex];
+  const Component = lazy(() => import(`../app/${CurrentComponent}.jsx`));
 
-    const cycleToNextComponent = () => {
-        setCurrentComponentIndex((currentComponentIndex + 1) % components.length);
-    }
+  const cycleToNextComponent = () => {
+      setCurrentComponentIndex(
+          (currentComponentIndex + 1) % components.length
+      );
+  };
 
-    useEffect(() => {
-        const timer = setInterval(cycleToNextComponent, 5000);
-        return () => clearInterval(timer);
-    }, [currentComponentIndex]);
+  useEffect(() => {
+      const timer = setInterval(cycleToNextComponent, 5000);
+      return () => clearInterval(timer);
+  }, [currentComponentIndex]);
 
-    return (
-        <>
-            <CurrentComponent />
-        </>
-    );
+  return (
+      <Suspense fallback={<LoadingComponent />}>
+          <Component />
+      </Suspense>
+  );
 }
 
 export default Cycle;
