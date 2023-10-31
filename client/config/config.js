6fad30de3af051e3131fd6b012de0d43693e055f
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-31 16:02:53 -0600

    added config for music tracks and paths

--- client/config/config.js
+++ client/config/config.js
@@ -30,3 +30,43 @@ export const pages = [
         path: "/LocalForecast",
     },
 ];
+
+/**
+ * `tracks` is an array representing each track for music player.
+ *  Each track contains their URL path.
+ */
+export const tracks = [
+    { path: '/music/1.mp3' },
+    { path: '/music/2.mp3' },
+    { path: '/music/3.mp3' },
+    { path: '/music/4.mp3' },
+    { path: '/music/5.mp3' },
+    { path: '/music/6.mp3' },
+    { path: '/music/7.mp3' },
+    { path: '/music/8.mp3' },
+    { path: '/music/9.mp3' },
+    { path: '/music/10.mp3' },
+    { path: '/music/11.mp3' },
+    { path: '/music/12.mp3' },
+    { path: '/music/13.mp3' },
+    { path: '/music/14.mp3' },
+    { path: '/music/15.mp3' },
+    { path: '/music/16.mp3' },
+    { path: '/music/17.mp3' },
+    { path: '/music/18.mp3' },
+    { path: '/music/19.mp3' },
+    { path: '/music/20.mp3' },
+    { path: '/music/21.mp3' },
+    { path: '/music/22.mp3' },
+    { path: '/music/23.mp3' },
+    { path: '/music/24.mp3' },
+    { path: '/music/25.mp3' },
+    { path: '/music/26.mp3' },
+    { path: '/music/27.mp3' },
+    { path: '/music/28.mp3' },
+    { path: '/music/29.mp3' },
+    { path: '/music/30.mp3' },
+    { path: '/music/31.mp3' },
+    { path: '/music/32.mp3' },
+    { path: '/music/33.mp3' },
+];
