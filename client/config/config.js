386f5e8298d735c86f4b5aca7a4c0693311ebe44
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-31 03:30:41 -0600

    added comments to config

--- client/config/config.js
+++ client/config/config.js
@@ -1,10 +1,18 @@
+// Defines the API endpoint for making requests.
 export const API_ENDPOINT = "localhost:8080";
 
+/**
+ * `pages` is an array representing each page in the application.
+ * Each page object contains the following properties:
+ * - `component`: The name of the React component associated with the page.
+ * - `title`: The title displayed in the navigation menu for the page.
+ * - `path`: The URL path for accessing the page.
+ */
 export const pages = [
     {
         component: "CompleteForecast",
         title: "Complete Forecast",
-        path: "/CompleteForecast"
+        path: "/CompleteForecast",
     },
     {
         component: "CurrentConditionsComponent",
