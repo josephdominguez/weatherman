2c35fc807df32365a27d1b1f87c46b37d97f3990
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-10-31 21:16:59 -0600

    created dynamically rendered local forecast

--- client/src/components/app/LocalForecastCard.jsx
+++ client/src/components/app/LocalForecastCard.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import styles from '@css/local_forecast.module.css';
 
-const LocalForecastCard = () => {
+const LocalForecastCard = ( {weatherData} ) => {
     return (
         <div className={styles['card-container']}>
             <div className={styles['card-item']}>
                 <div className={styles['forecast']}>
-                    .SYNOPSIS...A COLD EARLY SEASON STORM SYSTEM WILL MOVE THROUGH
-                    UTAH AND SOUTHWEST WYOMING TONIGHT THROUGH THURSDAY. THIS WILL
-                    BRING VALLEY RAIN AND SIGNIFICANT MOUNTAIN SNOW TO PRIMARILY
-                    NORTHERN UTAH, ALONG WITH THE COLDEST TEMPERATURES OF THE FALL
-                    SEASON SO FAR. HIGH PRESSURE WILL RETURN FRIDAY, BRINGING A
-                    GRADUAL WARMING TREND THROUGH THE WEEKEND. {' '}
-                    &&
-                    .SHORT TERM (THROUGH 12Z THURSDAY)...A BROAD STORM SYSTEM IS
-                    MOVING ONSHORE ALONG
+                    SYNOPSIS. {weatherData.synopsis}
                 </div>
             </div>
         </div>
--- client/src/components/app/LocalForecastComponent.jsx
+++ client/src/components/app/LocalForecastComponent.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import LocalForecastCard from '@components/app/LocalForecastCard';
+import ErrorComponent from '@components/app/ErrorComponent';
+import LoadingComponent from '@components/app/LoadingComponent';
+import { useLocation } from '@contexts/LocationContext';
+import { API_ENDPOINT } from '@config/config';
 
 function LocalForecastComponent() {
-    return <LocalForecastCard />;
+    const [weatherData, setWeatherData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const { location } = useLocation();
+    const { zipCode } = location;
+
+    const fetchWeatherData = async () => {
+        try {
+            const response = await axios.get(`http://${API_ENDPOINT}/local-forecast?zipCode=${zipCode}`);
+            const weatherData = response.data.localForecast;
+            setWeatherData(weatherData);
+            setError(null);
+            setLoading(false);
+        } catch(e) {
+            setError(e);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchWeatherData();
+    }, [zipCode]);
+
+    if (loading) {
+        return (
+            <LoadingComponent />
+        );
+    }
+
+    if (error) {
+        return (
+            <ErrorComponent error={error}>
+            </ErrorComponent>
+        );
+    }
+
+    return (
+        <LocalForecastCard weatherData={weatherData} />
+    );
 }
 
 export default LocalForecastComponent;
