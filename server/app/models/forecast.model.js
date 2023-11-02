313d54bf70dd176d64e02edb9b1f3c2b1abc43aa
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-02 13:55:04 -0600

    refactored forecast.model.js

--- server/app/models/forecast.model.js
+++ server/app/models/forecast.model.js
@@ -1,23 +1,24 @@
-exports.extractSynopsis = (forecastText) => {
-    const synopsisRegExp = /SYNOPSIS([\s\S]*?)&&/g;
-    const match = synopsisRegExp.exec(forecastText);
-
+/**
+ * Extracts a specific section of forecast from the given input text based on a section identifier.
+ *
+ * @param {string} section - The section identifier to search for in the text.
+ * @param {string} text - The text from which to extract the section.
+ * @returns {string} - The extracted section text, or an empty string if not found.
+ */
+_extractForecastSection = (section, text) => {
+    const regExp = new RegExp(`${section}([\\s\\S]*?)&&`, "g");
+    const match = regExp.exec(text);
     if (match) {
         const extractedText = match[1].trim();
         return extractedText;
-    } else {
-        return "";
     }
-}
+    return "";
+};
 
-exports.extractShortTerm = (forecastText) => {
-    const shortTermRegExp = /SHORT TERM([\s\S]*?)&&/g;
-    const match = shortTermRegExp.exec(forecastText);
-  
-    if (match) {
-      const extractedText = match[1].trim();
-      return extractedText;
-    } else {
-      return "";
-    }
-}
+exports.extractSynopsis = (text) => {
+    return _extractForecastSection("SYNOPSIS", text);
+};
+
+exports.extractShortTerm = (text) => {
+    return _extractForecastSection("SHORT TERM", text);
+};
