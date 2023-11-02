fcb14dc0902d2fd975878352827afd0462b0192b
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-02 12:12:28 -0600

    created generic apppagecomponent;

---
+++ client/src/components/app/AppPageComponent.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import LoadingComponent from '@components/app/LoadingComponent';
+import ErrorComponent from '@components/app/ErrorComponent';
+
+function AppPageComponent({ fetchFunction, renderData, zipCode }) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const fetchData = async () => {
+        try {
+            const data = await fetchFunction();
+            setData(data);
+            setError(null);
+            setLoading(false);
+        } catch (e) {
+            setError(e);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [zipCode]);
+
+    if (loading) {
+        return <LoadingComponent />;
+    }
+
+    if (error) {
+        return <ErrorComponent error={error} />;
+    }
+
+    return renderData(data);
+}
+
+export default AppPageComponent;
