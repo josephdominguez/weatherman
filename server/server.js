3658a313e7ed6b15454e84480aecf13dd4e28c20
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-03 01:14:22 -0600

    properly exports server

--- server/server.js
+++ server/server.js
@@ -29,6 +29,8 @@ app.get('/', (req, res) => {
 });
 
 // Starts server.
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+module.exports = server;
