259035e87b4e3df74cc0f34c56602d4de0c6aa24
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-04 03:42:50 -0600

    added eslint to backend

--- server/tests/config.js
+++ server/tests/config.js
@@ -3,11 +3,11 @@ const supertest = require("supertest");
 
 // Defines needed modules.
 const request = supertest(server);
-const expect = require("chai").expect;
+const expect = require('chai').expect;
 
 // Defines valid zip code (Sandy, UT) and invalid zip code.
-const VALID_ZIP = "84093";
-const INVALID_ZIP = "00000";
+const VALID_ZIP = '84093';
+const INVALID_ZIP = '00000';
 
 module.exports = {
     request,
