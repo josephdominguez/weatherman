6f5c52ddcb1170266b78d474f6920d71e53aa1e1
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-04 14:42:37 -0600

    created unit tests for nws service

---
+++ server/tests/services/nws.spec.js
@@ -0,0 +1,40 @@
+const { expect, VALID_LOCATION, INVALID_LOCATION } = require('../config');
+const NWSService = require('../../app/services/nws.service');
+
+describe('NWSService', function() {
+    let nwsService;
+
+    before(function() {
+        nwsService = new NWSService();
+    });
+
+    describe('getForecastStation', function() {
+        it('returns the forecast station identifier for a valid location', async function() {
+            const lat = VALID_LOCATION.lat;
+            const lon = VALID_LOCATION.lon;
+            const station = await nwsService.getForecastStation(lat, lon);
+            expect(station).to.equal('SLC');
+        });
+
+        it('throws error for invalid location', async function() {
+            const lat = INVALID_LOCATION.lat;
+            const lon = INVALID_LOCATION.lon;
+            try { await nwsService.getForecastStation(lat, lon); }
+            catch (e) { expect(e.response.status).to.equal(400); }
+        });
+    });
+
+    describe('getAreaForecastDiscussionUrl', function() {
+        it('returns the url for accessing area forecast discussion for a valid station', async function() {
+            const station = VALID_LOCATION.station;
+            const url = await nwsService.getAreaForecastDiscussionUrl(station);
+            expect(url).to.exist;
+        });
+
+        it('throws error for invalid station', async function() {
+            const station = INVALID_LOCATION.station;
+            try { await nwsService.getAreaForecastDiscussionUrl(station); }
+            catch(e) { expect(e).to.be.an.instanceOf(TypeError); }
+        });
+    })
+});
