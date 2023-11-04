e05aed82fae7223424822d856dc383703d920107
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-04 14:42:25 -0600

    added valid and invalid locations

--- server/tests/config.js
+++ server/tests/config.js
@@ -9,9 +9,27 @@ const expect = require('chai').expect;
 const VALID_ZIP = '84093';
 const INVALID_ZIP = '00000';
 
+// Defines valid and invalid location objects.
+const VALID_LOCATION = {
+    'city': 'Sandy',
+    'zipCode': '84093',
+    'station': 'SLC',
+    'lat': 40,
+    'lon': -111,
+}
+const INVALID_LOCATION = {
+    'city': '',
+    'zipCode': '00000',
+    'station': 'XXX',
+    'lat': 999,
+    'lon': 999,
+}
+
 module.exports = {
     request,
     expect,
     VALID_ZIP,
     INVALID_ZIP,
+    VALID_LOCATION,
+    INVALID_LOCATION,
 };
