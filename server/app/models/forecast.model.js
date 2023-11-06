aaaf2a387f6c7fc1c6d662e88f9f2c33919c7313
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-06 03:29:46 -0700

    modified exports

--- server/app/models/forecast.model.js
+++ server/app/models/forecast.model.js
@@ -7,19 +7,19 @@
  * @param {string} text - The text from which to extract the section.
  * @returns {string} - The extracted section text, or an empty string if not found.
  */
-_extractForecastSection = (section, text) => {
+exports.extractForecastSection = (section, text) => {
     const regExp = new RegExp(`${section}([\\s\\S]*?)&&`, 'g');
     const match = regExp.exec(text);
     if (match) {
         const extractedText = match[1].trim();
         return extractedText;
     }
-    return "";
+    return '';
 };
 
 exports.extractSynopsis = (text) => {
-    return _extractForecastSection('SYNOPSIS', text);};
+    return exports.extractForecastSection('SYNOPSIS', text);};
 
 exports.extractShortTerm = (text) => {
-    return _extractForecastSection('SHORT TERM', text);
+    return exports.extractForecastSection('SHORT TERM', text);
 };
