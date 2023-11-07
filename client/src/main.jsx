ba9f197718a594287eb55519318cd6c9dc6d7752
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-07 00:12:18 -0700

    created auth0 provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { Auth0ProviderWithNavigate } from './auth0-provider-with-navigate';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <BrowserRouter>
+            <Auth0ProviderWithNavigate>
+                <App />
+            </Auth0ProviderWithNavigate>
+        </BrowserRouter>
+    </React.StrictMode>
+);
