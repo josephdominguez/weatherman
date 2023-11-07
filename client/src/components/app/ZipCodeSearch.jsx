4f572940f9ceebd4f6b62f2a3660318481a074cb
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-06 22:28:11 -0700

    created error state for zip code search

--- client/src/components/app/ZipCodeSearch.jsx
+++ client/src/components/app/ZipCodeSearch.jsx
@@ -4,8 +4,7 @@ import { ZipCodeManager } from '@components/utilities/ZipCodeManager';
 import styles from '@css/app/zipcode_search.module.css';
 
 function ZipCodeSearch() {
-    const { handleInputChange, handleKeyPress, updateZipCode } =
-        ZipCodeManager();
+    const { handleInputChange, handleKeyPress, updateZipCode, error } = ZipCodeManager();
     const navigate = useNavigate();
 
     // Navigates to complete forecast if zip code is valid.
@@ -16,18 +15,21 @@ function ZipCodeSearch() {
     return (
         <>
             <div className={styles['search-container']}>
-                <GoSearch
-                    className={styles['search-svg']}
-                    onClick={() => updateZipCode(callback)}
-                />{' '}
-                <input
-                    className={styles['search-input']}
-                    type='text'
-                    placeholder='Get latest forecast by ZIP code'
-                    onChange={handleInputChange}
-                    onKeyDown={(event) => handleKeyPress(event, callback)}
-                />
+                <div className={styles['search-box-container']}>
+                    <GoSearch
+                        className={styles['search-svg']}
+                        onClick={() => updateZipCode(callback)}
+                    />
+                    <input
+                        className={`${styles['default-input']} ${error ? styles['error-input'] : styles['search-input']}`}
+                        type='text'
+                        placeholder='Get latest forecast by ZIP code'
+                        onChange={handleInputChange}
+                        onKeyDown={(event) => handleKeyPress(event, callback)}
+                    />
+                </div>
             </div>
+
         </>
     );
 }
--- client/src/components/utilities/ZipCodeManager.jsx
+++ client/src/components/utilities/ZipCodeManager.jsx
@@ -4,39 +4,43 @@ import { useLocation } from '@contexts/LocationContext';
 import { API_ENDPOINT } from '@config/config';
 
 export function ZipCodeManager() {
-  const { location, updateLocation } = useLocation();
-  const [tempZipCode, setTempZipCode] = useState(location.zipCode || '');
+    const { location, updateLocation } = useLocation();
+    const [tempZipCode, setTempZipCode] = useState(location.zipCode || '');
+    const [error, setError] = useState(null);
 
-  const handleInputChange = (event) => {
-    setTempZipCode(event.target.value);
-  };
+    const handleInputChange = (event) => {
+        setTempZipCode(event.target.value);
+    };
 
-  const handleKeyPress = (event, onSuccessCallback) => {
-    if (event.key === 'Enter') {
-      updateZipCode(onSuccessCallback);
-    }
-  };
+    const handleKeyPress = (event, onSuccessCallback) => {
+        if (event.key === 'Enter') {
+            updateZipCode(onSuccessCallback);
+        }
+    };
 
-  const updateZipCode = async (onSuccessCallback) => {
-    try {
-      const response = await axios.get(
-        `http://${API_ENDPOINT}/location?zipCode=${tempZipCode}`
-      );
-      const location = response.data.location;
-      updateLocation({ ...location });
+    const updateZipCode = async (onSuccessCallback) => {
+        try {
+            const response = await axios.get(
+                `http://${API_ENDPOINT}/location?zipCode=${tempZipCode}`
+            );
+            const location = response.data.location;
+            updateLocation({ ...location });
+            setError(null);
 
-      if (onSuccessCallback) {
-        onSuccessCallback();
-      }
-    } catch (e) {
-      updateLocation({ zipCode: tempZipCode });
-    }
-  };
+            if (onSuccessCallback) {
+                onSuccessCallback();
+            }
+        } catch (e) {
+            setError('Invalid ZIP code.');
+            updateLocation({ zipCode: tempZipCode });
+        }
+    };
 
-  return {
-    tempZipCode,
-    handleInputChange,
-    handleKeyPress,
-    updateZipCode,
-  };
+    return {
+        tempZipCode,
+        handleInputChange,
+        handleKeyPress,
+        updateZipCode,
+        error
+    };
 }
