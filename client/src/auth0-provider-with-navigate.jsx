eb4498ef973135f9de7d961d7482731a636a1869
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-08 13:00:23 -0700

    added audience url

--- client/src/auth0-provider-with-navigate.jsx
+++ client/src/auth0-provider-with-navigate.jsx
@@ -7,12 +7,13 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
   };
 
-  if (!(domain && clientId && redirectUri)) {
+  if (!(domain && clientId && redirectUri && audience)) {
     return null;
   }
 
@@ -21,6 +22,7 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
       domain={domain}
       clientId={clientId}
       authorizationParams={{
+        audience: audience,
         redirect_uri: redirectUri,
       }}
       onRedirectCallback={onRedirectCallback}
