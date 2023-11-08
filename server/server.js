0c97794e5fd95b1b6d7cd0c42fb6530fc0e4370c
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-07 20:21:01 -0700

    loaded in db and passport, added user routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const Auth0Strategy = require('passport-auth0');
 const cors = require('cors');
+const passport = require('passport');
 
 // Loads environment variables.
 require('dotenv').config();
+
+// Loads MongoDB connection code.
+require('./app/models/db.model.js');
+
+// Loads Passport/Auth0 configuration.
+require('./app/config/passport.config.js');
 
 // Defines port for Express server.
 const PORT = process.env.PORT || 8080;
@@ -21,40 +26,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
-
-// Passport/Auth0 configuration.
-const strategy = new Auth0Strategy(
-    {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        callbackURL: process.env.AUTH0_CALLBACK_URL,
-    },
-    function (accessToken, refreshToken, extraParams, profile, done) {
-        /**
-         * Access tokens are used to authorize users to an API
-         * (resource server)
-         * accessToken is the token to call the Auth0 API
-         * or a secured third-party API
-         * extraParams.id_token has the JSON Web Token
-         * profile has all the information from the user
-         */
-        return done(null, profile);
-    }
-);
-passport.use(strategy);
 app.use(passport.initialize());
-
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-    done(null, user);
-});
 
 // Loads routes.
 require('./app/routes/weather.routes.js')(app);
+require('./app/routes/users.routes')(app);
 
 // Creates default route.
 app.get('/', (req, res) => {
