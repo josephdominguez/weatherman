c95237738b8d7f19f8ac704cf90f2de1857bedd1
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-13 23:23:20 -0700

    implemented blank app page

--- client/src/components/pages/NotFound.jsx
+++ client/src/components/pages/NotFound.jsx
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom';
+import BlankAppPage from '@components/pages/BlankAppPage';
 import styles from '@css/not_found.module.css';
 
 function NotFound() {
     return (
-        <>
-            <main className={styles['not-found-page']}>
-                <div className={styles['not-found-container']}>
-                    <h2>404 - Page Not Found</h2>
-                    <Link to='/'>Go back to the homepage</Link>
-                </div>
-            </main>
-        </>
+        <BlankAppPage>
+            <div className={styles['not-found-container']}>
+                <h2>404 - Page Not Found</h2>
+                <Link to='/'>Go back to the homepage</Link>
+            </div>
+        </BlankAppPage>
     );
 }
 
