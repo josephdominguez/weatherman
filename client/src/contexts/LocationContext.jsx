515aca861262cb09a4aec9f9471796c795badf48
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-14 03:47:08 -0700

    minor syntax correction

--- client/src/contexts/LocationContext.jsx
+++ client/src/contexts/LocationContext.jsx
@@ -5,39 +5,41 @@ import { API_ENDPOINT } from '@config/config';
 export const LocationContext = createContext();
 
 export function useLocation() {
-  return useContext(LocationContext);
+    return useContext(LocationContext);
 }
 
 export function LocationProvider({ children }) {
-  const [location, setLocation] = useState({
-    zipCode: '84101',
-    city: 'Salt Lake City',
-  });
+    const [location, setLocation] = useState({
+        zipCode: '84101',
+        city: 'Salt Lake City',
+    });
 
-  const updateLocation = (newLocation) => {
-    setLocation((prevLocation) => ({ ...prevLocation, ...newLocation }));
-  };
+    const updateLocation = (newLocation) => {
+        setLocation((prevLocation) => ({ ...prevLocation, ...newLocation }));
+    };
 
-  const updateLocationByZipCode = async (zipCode, onSuccessCallback) => {
-    try {
-      const response = await axios.get(
-        `http://${API_ENDPOINT}/location?zipCode=${zipCode}`
-      );
-      const newLocation = response.data.location;
-      updateLocation(newLocation);
+    const updateLocationByZipCode = async (zipCode, onSuccessCallback) => {
+        try {
+            const response = await axios.get(
+                `http://${API_ENDPOINT}/location?zipCode=${zipCode}`
+            );
+            const newLocation = response.data.location;
+            updateLocation(newLocation);
 
-      if (onSuccessCallback) {
-        onSuccessCallback();
-      }
-    } catch (e) {
-      console.error('Error updating ZIP code:', e);
-      throw new Error('Invalid ZIP code.');
-    }
-  };
+            if (onSuccessCallback) {
+                onSuccessCallback();
+            }
+        } catch (e) {
+            console.error('Error updating ZIP code:', e);
+            throw new Error('Invalid ZIP code.');
+        }
+    };
 
-  return (
-    <LocationContext.Provider value={{ location, updateLocation, updateLocationByZipCode }}>
-      {children}
-    </LocationContext.Provider>
-  );
+    return (
+        <LocationContext.Provider
+            value={{ location, updateLocation, updateLocationByZipCode }}
+        >
+            {children}
+        </LocationContext.Provider>
+    );
 }
