9ebf1589e6928110d368ded2a2740aefed13d9c5
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-13 23:23:49 -0700

    created and styled onboarding page

--- client/src/components/pages/Onboarding.jsx
+++ client/src/components/pages/Onboarding.jsx
@@ -3,6 +3,8 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 import { useUserInfo } from '@contexts/UserInfoContext';
 import UserAPI from '@components/utilities/UserAPI';
+import BlankAppPage from '@components/pages/BlankAppPage';
+import styles from '@css/onboarding.module.css';
 
 function Onboarding() {
     const { user } = useAuth0();
@@ -12,43 +14,58 @@ function Onboarding() {
     const [zipCode, setZipCode] = useState('');
     const [unitPreference, setUnitPreference] = useState('imperial');
 
-    const handleOnboardingSubmit = async () => {
-        // Perform any additional validation or data processing here
+    const handleOnboardingSubmit = async (e) => {
+        e.preventDefault();
+
         const userInfo = { sub: user.sub, email: user.email, zipCode, unitPreference };
         updateUserInfo(userInfo);
 
-        // Save the user info to the server.
         createUserWithAPI(userInfo);
 
-        // Redirect to the desired page (e.g., CompleteForecast)
         navigate('/CompleteForecast');
     };
 
     return (
-        <div>
-            <h1>Welcome to our app!</h1>
-            <form onSubmit={handleOnboardingSubmit}>
-                <label>
-                    Zip Code:
-                    <input
-                        type='text'
-                        value={zipCode}
-                        onChange={(e) => setZipCode(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Unit Preference:
-                    <select
-                        value={unitPreference}
-                        onChange={(e) => setUnitPreference(e.target.value)}
+        <BlankAppPage>
+            <div className={styles['card-container']}>
+                <div className={styles['onboarding-container']}>
+                    <h1 className={styles['onboarding-header']}>Account Setup</h1>
+                    <form className={styles['onboarding-form']}
+                        onSubmit={handleOnboardingSubmit}
                     >
-                        <option value='imperial'>Imperial</option>
-                        <option value='metric'>Metric</option>
-                    </select>
-                </label>
-                <button type='submit'>Complete Onboarding</button>
-            </form>
-        </div>
+                        <div className={styles['onboarding-form-group']}>
+                            <label className={styles['onboarding-form-label']}>
+                                Zip Code:
+                                <input
+                                    type='text'
+                                    value={zipCode}
+                                    onChange={(e) => setZipCode(e.target.value)}
+                                    className={styles['onboarding-form-input']}
+                                />
+                            </label>
+                        </div>
+
+                        <div className={styles['onboarding-form-group']}>
+                            <label className={styles['onboarding-form-label']}>
+                                Unit Preference:
+                                <select
+                                    value={unitPreference}
+                                    onChange={(e) => setUnitPreference(e.target.value)}
+                                    className={styles['onboarding-form-select']}
+                                >
+                                    <option value='imperial'>Imperial</option>
+                                    <option value='metric'>Metric</option>
+                                </select>
+                            </label>
+                        </div>
+
+                        <button type='submit' className={styles['onboarding-form-button']}>
+                            Complete Onboarding
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </BlankAppPage>
     );
 }
 
