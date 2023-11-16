4e7a6f5cab4d587f7d2c3a14dabe81a9f6308256
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-16 01:20:54 -0700

    implemented validation for user routes

--- server/app/routes/users.routes.js
+++ server/app/routes/users.routes.js
@@ -1,21 +1,23 @@
 const userController = require('../controllers/user.controller.js');
 const checkJwt = require('../config/auth.config.js');
+const { validateUserInfo, validateUserSub } = require('./validators.js');
+
 
 module.exports = app => {
     // Create a new user.
-    app.post('/users', checkJwt, async (req, res) => {
+    app.post('/users', checkJwt, validateUserInfo, async (req, res) => {
         try { await userController.createUser(req, res); }
         catch(e) { throw e; }
     });
 
     // Retrieve user data.
-    app.get('/users', checkJwt, async (req, res) => {
+    app.get('/users', checkJwt, validateUserSub, async (req, res) => {
         try { await userController.getUser(req, res); }
         catch(e) { throw e; }
     });
 
     // Update existing user.
-    app.put('/users', checkJwt, async (req, res) => {
+    app.put('/users', checkJwt, validateUserInfo, async (req, res) => {
         try { await userController.updateUser(req, res); }
         catch(e) { throw e; }
     })
--- server/app/routes/validators.js
+++ server/app/routes/validators.js
@@ -1,9 +1,21 @@
-const { validationResult, query } = require('express-validator');
+const { validationResult, query, body } = require('express-validator');
+
+// Common validation middleware
+const validate = (validations) => [
+    ...validations,
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ message: errors.array()[0].msg });
+        }
+        next();
+    },
+];
 
 // Custom validation function for a valid ZIP code (5 or 9 digits)
 const isValidZipCodePattern = (value) => {
-    const zipCode5Pattern = /^\d{5}$/;
-    const zipCode9Pattern = /^\d{5}-\d{4}$/;
+    const zipCode5Pattern = /^\d{5}$/; // XXXXX format zip code
+    const zipCode9Pattern = /^\d{5}-\d{4}$/; // XXXXX-XXXX format zip code
 
     if (!(zipCode5Pattern.test(value) || zipCode9Pattern.test(value))) {
         throw new Error(
@@ -13,16 +25,40 @@ const isValidZipCodePattern = (value) => {
     return true;
 };
 
-// Validation middleware for ZIP code
-const validateZipCode = [
-    query('zipCode').custom(isValidZipCodePattern),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        next();
-    },
-];
+// Custom validation function for savedLocations
+const isValidSavedLocations = (value) => {
+    if (typeof value === 'string') {
+        return isValidZipCodePattern(value);
+    } else if (
+        Array.isArray(value) &&
+        value.every((item) => typeof item === 'string' && isValidZipCodePattern(item))
+    ) {
+        return true;
+    } else {
+        throw new Error(
+            'Invalid locations. Should be a single ZIP code or a list of valid ZIP codes.'
+        );
+    }
+};
 
-module.exports = { validateZipCode };
+const validateZipCode = validate([
+    query('zipCode').custom(isValidZipCodePattern)
+]);
+
+const validateUserSub = validate([
+    body('sub').notEmpty().withMessage('Sub cannot be empty.'),
+]);
+
+const validateUserInfo = validate([
+    body('sub').notEmpty().withMessage('Sub cannot be empty.'),
+    body('email').isEmail().withMessage('Invalid email format.'),
+    body('savedLocations').custom(isValidSavedLocations),
+    body('unitPreference').isIn(['imperial', 'metric'])
+        .withMessage('Invalid unitPreference. Must be "imperial" or "metric".'),
+]);
+
+module.exports = {
+    validateZipCode,
+    validateUserSub,
+    validateUserInfo,
+};
