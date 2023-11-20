f25719cb0a1f416d649e3fa42973c66e501076f9
josephdominguez <joseph.dominguez.virla@gmail.com>
2023-11-20 04:24:49 -0700

    minor syntax correction

--- client/src/components/auth/buttons/LogoutButton.jsx
+++ client/src/components/auth/buttons/LogoutButton.jsx
@@ -1,13 +1,11 @@
 import LogUserOut from '@components/auth/LogUserOut';
 
 function LogoutButton() {
-  return (
-    <LogUserOut>
-      <button className="logout-button">
-        Log Out
-      </button>
-    </LogUserOut>
-  );
+    return (
+        <LogUserOut>
+            <button className='logout-button'>Log Out</button>
+        </LogUserOut>
+    );
 }
 
 export default LogoutButton;
