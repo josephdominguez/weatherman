9b553a3f25ad88e657f5c91c0b5a4822c5d66270
josephdominguez <joseph.dominguez.virla@gmail.com>
2024-03-12 16:52:32 -0600

    updated card loading component with spinner

--- client/src/components/app/LoadingComponent.jsx
+++ client/src/components/app/LoadingComponent.jsx
@@ -1,10 +1,13 @@
+import Spinner from '@components/utilities/Spinner';
 import styles from '@css/app/card_container.module.css';
 
 function LoadingComponent() {
     return (
         <div className={`${styles['card-container']}`}>
             <div className={styles['card-item']}>
-                <div>Loading...</div>
+                <div className={styles['loading-card-container']}>
+                    <Spinner />
+                </div>
             </div>
         </div>
     );
