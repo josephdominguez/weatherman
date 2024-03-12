71eab2f71caf064435c97fdf2d7c660eda827775
josephdominguez <joseph.dominguez.virla@gmail.com>
2024-03-12 14:30:18 -0600

    created object to map long conditions to abbreviations

---
+++ server/app/models/abbreviatedConditions.js
@@ -0,0 +1,12 @@
+// Abbreviations for conditions.
+
+/**
+ * This object contains mappings of weather conditions to their abbreviations.
+ *
+ * @type {Object.<string, string>}
+ */
+const abbreviatedConditions = {
+    'Partly cloudy': 'P Cloudy',
+};
+
+module.exports = { abbreviatedConditions };
