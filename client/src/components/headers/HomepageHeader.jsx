f92eb6d7732320f86a8b22e3dee83a9359732dd7
josephdominguez <joseph.dominguez.virla@gmail.com>
2024-03-12 16:41:15 -0600

    improved button theming

--- client/src/components/headers/HomepageHeader.jsx
+++ client/src/components/headers/HomepageHeader.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react';
 import SignupButton from '@components/auth/buttons/SignupButton';
 import LoginButton from '@components/auth/buttons/LoginButton';
 import ZipCodeSearch from '@components/app/ZipCodeSearch';
@@ -9,39 +9,42 @@ import HeroVideo from '@videos/hero_video.mp4';
 import styles from '@css/homepage/header.module.css';
 
 function HomepageHeader() {
-  const { isAuthenticated } = useAuth0();
+    const { isAuthenticated } = useAuth0();
 
-  return (
-    <header className={styles['homepage-header']}>
-        <div className={styles['background-video']}>
-          <video autoPlay muted loop className={styles['header-video']}>
-            <source src={HeroVideo} type="video/mp4" />
-          </video>
-        </div>
-        <div className={styles['header-container']}>
-
-        <div className={styles['logo']}>
-          <Link to="/">
-            <img className={styles['logo-image']} src={WeathermanLogo} alt="Weather Logo" />
-          </Link>
-        </div>
-        <div>
-          {! isAuthenticated && (
-            <>
-              <SignupButton />
-              <LoginButton />
-            </>
-          )}
-          {isAuthenticated && (
-            <Link to="/CompleteForecast">
-              <GoPersonFill className={styles['user-button']} />
-            </Link>
-          )}
-        </div>
-      </div>
-      <ZipCodeSearch />
-    </header>
-  );
+    return (
+        <header className={styles['homepage-header']}>
+            <div className={styles['background-video']}>
+                <video autoPlay muted loop className={styles['header-video']}>
+                    <source src={HeroVideo} type='video/mp4' />
+                </video>
+            </div>
+            <div className={styles['header-container']}>
+                <div className={styles['logo']}>
+                    <Link to='/'>
+                        <img
+                            className={styles['logo-image']}
+                            src={WeathermanLogo}
+                            alt='Weather Logo'
+                        />
+                    </Link>
+                </div>
+                <div className={styles['auth-container']}>
+                    {!isAuthenticated && (
+                        <>
+                            <SignupButton />
+                            <LoginButton />
+                        </>
+                    )}
+                    {isAuthenticated && (
+                        <Link to='/CompleteForecast'>
+                            <GoPersonFill className={styles['user-button']} />
+                        </Link>
+                    )}
+                </div>
+            </div>
+            <ZipCodeSearch />
+        </header>
+    );
 }
 
 export default HomepageHeader;
